ab94961d150e54345d2893fce7245fac18f9269b
ZarLiHnin <zarlehninn@gmail.com>
2025-06-25 17:11:31 +0900

    performanace fixx

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default async function HomePage() {
         <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
           <a
             href="https://mail.google.com/mail/?view=cm&fs=1&to=zarlehninn@gmail.com"
-            className="flex items-center gap-2 text-yellow-600 hover:underline"
+            className="flex items-center gap-2 text-yellow-800 hover:underline hover:text-yellow-600"
           >
             <Mail className="w-5 h-5" />
             zarlehninn@gmail.com
@@ -48,7 +48,7 @@ export default async function HomePage() {
 
           <a
             href="tel:+819012345678"
-            className="flex items-center gap-2 text-yellow-600 hover:underline"
+            className="flex items-center gap-2 text-yellow-800 hover:underline hover:text-yellow-600"
           >
             <Phone className="w-5 h-5" />
             090-1234-5678
@@ -58,7 +58,7 @@ export default async function HomePage() {
             href="https://www.facebook.com/zarle.hninn/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2 text-yellow-600 hover:underline"
+            className="flex items-center gap-2 text-yellow-800 hover:underline hover:text-yellow-600"
           >
             <Facebook className="w-5 h-5" />
             Facebookで連絡
