ad955552233e0f3b50234fa4d684cb1e99118880
ZarLiHnin <zarlehninn@gmail.com>
2025-06-26 11:19:47 +0900

    performanace fixx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,14 @@
+import type { Metadata } from "next";
 import ProfileCard from "@/components/ProfileCard";
 import SkillSet from "@/components/SkillSet";
 import AnimatedContainer from "@/components/AnimatedContainer"; // 後述のクライアントコンポーネント
 import { getGroupedSkills } from "@/lib/skills";
+
+export const metadata: Metadata = {
+  title: "Zar Li Portfolio | プロフェル",
+  description:
+    "Zar Liのポートフォリオサイト。開発実績やスキル情報を紹介しています。",
+};
 
 export default async function ProfilePage() {
   const groupedSkills = await getGroupedSkills();
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,7 +1,14 @@
 // app/works/page.tsx
+import type { Metadata } from "next";
 export const revalidate = 60;
 import WorksList from "@/components/WorksList";
 import { getAllWorks } from "@/lib/works";
+
+export const metadata: Metadata = {
+  title: "Zar Li Portfolio | 制作実績一覧",
+  description:
+    "Zar Liのポートフォリオサイト。開発実績やスキル情報を紹介しています。",
+};
 
 export default async function WorksPage() {
   const works = await getAllWorks();
